4cdf116c333de48d0de2a23d7a1182f47c488e2a
BalssemZoghbiKeyrus <balssem.zoghbi@keyrus.com>
2024-05-06 11:18:39 +0200

    first-commit

---
+++ pages/index.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <h2>Home</h2>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ea beatae pariatur non nulla voluptatibus tenetur consequuntur, aliquid repellat consequatur ipsam reiciendis. Magni recusandae mollitia quibusdam earum quos, quasi amet.</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum ea beatae pariatur non nulla voluptatibus tenetur consequuntur, aliquid repellat consequatur ipsam reiciendis. Magni recusandae mollitia quibusdam earum quos, quasi amet.</p>
+  </div>
+</template>
+
+<script setup>
+
+</script>
+
+<style>
+h2{
+    margin-bottom: 20px;
+    font-size: 36px;
+}
+p{
+    margin: 20px 0;
+}
+</style>
