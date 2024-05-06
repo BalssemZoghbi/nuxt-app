e62af1cf7d278ae08dfc647fd4b90a2f20cb275d
BalssemZoghbiKeyrus <balssem.zoghbi@keyrus.com>
2024-05-06 11:28:09 +0200

    Route params

---
+++ pages/products/[id].vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <p>Product details for {{ id }}</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dignissimos, suscipit officiis consequatur eos, vero, enim facilis nesciunt est quas placeat! Odio, rerum enim harum alias nostrum est cumque facilis?</p>
+  </div>
+</template>
+
+<script  setup>
+const { id } =useRoute().params
+</script>
+
+<style>
+
+</style>
--- pages/products/index.vue
+++ pages/products/index.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <p>Products</p>
+    
   </div>
 </template>
 
